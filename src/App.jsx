f850b9a7030ee618e07571d2a280f7f263533f7a
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-26 22:21:57 +0330

    fonts added to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
-
 function App() {
 
-  return (
-      <h1 className='flex justify-center text-center font-bold'>Admin dashboard application</h1>
-  )
+    return (
+        <h1 className='flex justify-center text-center font-bold text-gray-500'>پلتفرم آموزش آنلاین</h1>
+    )
 }
 
 export default App

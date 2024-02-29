239bd67d2c77f62602d0d7c1388caf2c9c18529b
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-29 23:07:02 +0330

    change-language component created

--- src/components/change-language.jsx
+++ src/components/change-language.jsx
@@ -1,10 +1,12 @@
 import usFlag from '@assets/images/us.png';
+import faFlag from '@assets/images/fa.png';
 
 const ChangeLanguage = () => {
     return (
         <div className='relative'>
             <a className='pt-1 pr-1 pb-1 pl-1 block rounded-b-full font-light bg-gray-700 transition-all delay-1000 ease-in-out leading-snug whitespace-nowrap'>
                 <img src={usFlag} alt='English'/>
+                <img src={faFlag} alt='Persian'/>
             </a>
         </div>
     )

83a1a341ff909ecc518fc60828a5261741fd24c5
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-01 22:02:54 +0330

    dropdown menu is creating

--- src/components/change-language.jsx
+++ src/components/change-language.jsx
@@ -7,7 +7,7 @@ const ChangeLanguage = () => {
             <a className='mt-0.5 font-light transition-all delay-1000 ease-in-out leading-snug whitespace-nowrap'>
                 <img src={usFlag} alt='English' className='rounded-full border border-gray-100 w-7 h-7'/>
             </a>
-            <div>
+            <div className='absolute z-[1000] hidden min-w-32 p-1 m-0 font-light text-left list-none bg-white bg-clip-padding border rounded-sm'>
                 <a>
                     <img src={faFlag} width='20px'/>
                     <span>فارسی</span>

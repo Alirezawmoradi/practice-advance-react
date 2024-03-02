f520383d77a8ef3ac6f637639b19cb6ceb17aa0f
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-02 20:50:15 +0330

    dropdown menu item completed

--- src/components/change-language.jsx
+++ src/components/change-language.jsx
@@ -7,14 +7,14 @@ const ChangeLanguage = () => {
             <a className='mt-0.5 font-light transition-all delay-1000 ease-in-out leading-snug whitespace-nowrap'>
                 <img src={usFlag} alt='English' className='rounded-full border border-gray-100 w-7 h-7'/>
             </a>
-            <div className='absolute z-[1000] hidden min-w-32 p-1 m-0 font-light text-left list-none bg-white bg-clip-padding border rounded-sm'>
-                <a>
+            <div className='block top-auto absolute z-auto min-w-40 p-2 m-0 font-light text-left list-none bg-clip-padding border rounded-md bg-white'>
+                <a className='flex items-center gap-2 text-center hover:bg-gray-100 hover:text-gray-800 cursor-pointer mb-2 mr-3'>
                     <img src={faFlag} width='20px'/>
-                    <span>فارسی</span>
+                    <span className='align-middle font-bold text-gray-500 text-sm'>فارسی</span>
                 </a>
-                <a>
+                <a className='flex items-center gap-2 text-center hover:bg-gray-100 cursor-pointer mb-2 hover:text-gray-800 mr-3'>
                     <img src={usFlag} width='20px'/>
-                    <span>English</span>
+                    <span className='align-middle font-bold text-gray-500 text-sm'>English</span>
                 </a>
             </div>
         </div>

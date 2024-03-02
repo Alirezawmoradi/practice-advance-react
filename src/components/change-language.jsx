2e7e5e1fd8304abc8dfc4ddfa9cc95fa3fad4ad0
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-02 23:04:11 +0330

    changing language using app context completed

--- src/components/change-language.jsx
+++ src/components/change-language.jsx
@@ -1,10 +1,13 @@
 import usFlag from '@assets/images/us.png';
 import faFlag from '@assets/images/fa.png';
 import {useEffect, useRef, useState} from "react";
+import {useAppContext} from "../contexts/app/app-context.jsx";
 
 const ChangeLanguage = () => {
     const [show, setShow] = useState(false);
     const ref = useRef();
+
+    const {language, changeLanguage} = useAppContext();
 
     useEffect(() => {
 
@@ -23,21 +26,25 @@ const ChangeLanguage = () => {
     }, [show]);
 
     return (
-        <div className='relative mr-5'>
+        <div className='relative mr-5 ml-5'>
             <a className='mt-0.5 font-light transition-all delay-1000 ease-in-out leading-snug whitespace-nowrap cursor-pointer'
                onClick={() => setShow(true)}>
-                <img src={usFlag} alt='English'
+                <img src={language === 'fa' ? faFlag : usFlag} alt='English'
                      className='rounded-full border border-gray-100 w-7 h-7'/>
             </a>
             <div
                 className={`${show ? 'block' : 'hidden'} top-auto absolute z-auto min-w-40 p-2 m-0 font-light text-left list-none bg-clip-padding border rounded-md bg-white`}
                 ref={ref}
             >
-                <a className='flex items-center gap-2 text-center hover:bg-gray-100 hover:text-gray-800 cursor-pointer mb-2 mr-3'>
+                <a className='flex items-center gap-2 text-center hover:bg-gray-100 hover:text-gray-800 cursor-pointer mb-2 mr-3'
+                   onClick={() => changeLanguage('fa')}
+                >
                     <img src={faFlag} width='20px'/>
                     <span className='align-middle font-bold text-gray-500 text-sm'>فارسی</span>
                 </a>
-                <a className='flex items-center gap-2 text-center hover:bg-gray-100 cursor-pointer mb-2 hover:text-gray-800 mr-3'>
+                <a className='flex items-center gap-2 text-center hover:bg-gray-100 cursor-pointer mb-2 hover:text-gray-800 mr-3'
+                   onClick={() => changeLanguage('en')}
+                >
                     <img src={usFlag} width='20px'/>
                     <span className='align-middle font-bold text-gray-500 text-sm'>English</span>
                 </a>
--- src/contexts/app/app-context.jsx
+++ src/contexts/app/app-context.jsx
@@ -1,5 +1,6 @@
-import {createContext, useContext, useReducer} from "react";
+import {createContext, useContext, useEffect, useReducer} from "react";
 import appReducer from "./app-reducer.js";
+import {useTranslation} from "react-i18next";
 
 const AppContext = createContext();
 
@@ -9,10 +10,16 @@ const initialState = {
 
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(appReducer, initialState);
+    const {i18n} = useTranslation();
 
     const changeLanguage = (language) => {
         dispatch({type: 'CHANGE_LANGUAGE', payload: language})
     }
+
+    useEffect(() => {
+        i18n.changeLanguage(state.language);
+        localStorage.setItem('language', state.language);
+    }, [state.language]);
 
     return <AppContext.Provider value={{...state, changeLanguage}}>
         {children}

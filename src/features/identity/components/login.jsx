5194a8c90966301ff4f2ec959c708727eed27316
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-03 23:01:34 +0330

    dark styled added to login page

--- src/features/identity/components/login.jsx
+++ src/features/identity/components/login.jsx
@@ -13,28 +13,28 @@ const Login = () => {
                     <div className='flex justify-center'>
                         <img src={logo} style={{height: '100px'}}/>
                     </div>
-                    <h1 className='text-center text-xl font-extrabold text-gray-600'>{t('login.title')}</h1>
-                    <p className='leading-normal mt-4 text-base font-light text-gray-500'>
+                    <h1 className='text-center text-xl font-extrabold text-gray-600 dark:text-white'>{t('login.title')}</h1>
+                    <p className='leading-normal mt-4 text-base font-light text-gray-500 dark:text-gray-400'>
                         {t('login.introMessage')}
                     </p>
-                    <p className='leading-normal mt-4 text-base font-light text-gray-500'>
+                    <p className='leading-normal mt-4 text-base font-light text-gray-500 dark:text-gray-400'>
                         {t('login.areNotRegistered')}
-                        <Link className='me-2 text-blue-600 mr-2' to='/register'>{t('login.register')} </Link>
+                        <Link className='me-2 text-blue-600 dark:text-blue-500 mr-2' to='/register'>{t('login.register')} </Link>
                     </p>
                 </div>
                 <div
-                    className='mb-4 shadow relative flex flex-col min-w-0 break-words bg-white border bg-clip-border mt-5 rounded-lg'>
+                    className='mb-4 shadow relative flex flex-col min-w-0 break-words bg-white dark:bg-card dark:border-none border bg-clip-border mt-5 rounded-lg'>
                     <div className='flex-auto p-2 block box-border'>
                         <div className='m-6 block box-bordex'>
                             <form>
                                 <div className='mb-3'>
                                     <label
-                                        className='inline-block box-border cursor-default text-gray-500 mb-2'>{t('login.mobile')}</label>
+                                        className='inline-block box-border cursor-default text-gray-500 dark:text-gray-400 mb-2'>{t('login.mobile')}</label>
                                     <input
                                         className='min-h-1 py-1 pl-1.5 pr-4 outline-offset-2 focus:outline-blue-300 bg-blue-100 border border-gray-300 rounded-md block w-full font-medium'/>
                                 </div>
                                 <div className='mb-3'>
-                                    <label className='inline-block box-border cursor-default text-gray-500 mb-2'>
+                                    <label className='inline-block box-border cursor-default text-gray-500 dark:text-gray-400 mb-2'>
                                         {t('login.password')}
                                     </label>
                                     <input

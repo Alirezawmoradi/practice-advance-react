0055c3804f8859fbbfeef4b17cb6cd87b9b4a21b
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-06 23:14:24 +0330

    actions created for login page

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,9 +1,19 @@
-import axios from "axios";
-import {httpService} from "../core/http-service.js";
+import {httpService} from "@core/http-service.js";
+import {redirect} from "react-router-dom";
 
 export async function registerAction({request}) {
     const formData = await request.formData();
-    const data = Object.fromEntries(formData);
+    const data = Object.fromEntries(formData); //to convert key values to object
     const response = await httpService.post('/Users', data);
     return response.status === 200;
+}
+
+export async function loginAction({request}) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData); //to convert key values to object
+    const response = await httpService.post('/Users/login', data);
+    if (response.status === 200) {
+        localStorage.setItem('token', response.data.token);
+        return redirect('/');
+    }
 }

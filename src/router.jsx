7929f4ffb78b84868f00a11091583793d2a350f7
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-06 23:17:37 +0330

    routes change for submitting form and redirect to dashboard

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,28 @@
 import {createBrowserRouter} from "react-router-dom";
 import Login from "./features/identity/components/login.jsx";
 import IdentityLayout from "./layouts/identity-layout.jsx";
-import {registerAction} from "./actions/actions.jsx";
+import {loginAction, registerAction} from "./actions/actions.jsx";
 import Register from "./features/identity/components/register.jsx";
+import {MainLayout} from "./layouts/main-layout.jsx";
+import {Courses} from "./pages/courses.jsx";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout/>,
+        children: [{
+            element: <Courses/>,
+            index: true
+        }]
+    },
     {
         element: <IdentityLayout/>,
         children: [
             {
                 path: 'login',
-                element: <Login/>
+                element: <Login/>,
+                action: loginAction,
+                errorElement: <Login/>,
             }, {
                 path: 'register',
                 element: <Register/>,

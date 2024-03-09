b28212d0bfde433037010cd7e99ef5539eb7e19c
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-09 20:10:08 +0330

    sidebar created

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,18 +1,21 @@
 import {Outlet} from "react-router-dom";
-
+import logo from '@assets/images/logo.svg';
 export const MainLayout = () => {
     return (
-        <div className='flex-wrap min-h-screen'>
-            <nav className='min-w-64 max-w-64 bg-dark'>
+        <div className='items-stretch flex min-h-screen'>
+            <nav className='min-w-64 max-w-64 bg-gray-800 transition-all ease-in-out duration-300'>
 
             </nav>
             <div className='flex w-full min-h-screen min-w-0 flex-col'>
                 <nav
                     className='relative flex py-3.5 px-0.5 items-center border-b-0 shadow justify-start bg-white align-middle'>
                     <a className='cursor-pointer w-6 h-6 flex flex-col justify-center space-y-1.5 px-5'>
-                        <span className='cursor-pointer rounded-sm h-0.5 w-5 bg-gray-600 block content-none transition-all duration-1000 ease-in-out'></span>
-                        <span className='cursor-pointer rounded-sm h-0.5 w-6 bg-gray-600 block content-none transition-all duration-1000 ease-in-out'></span>
-                        <span className='cursor-pointer rounded-sm h-0.5 w-4 bg-gray-600 block content-none transition-all duration-1000 ease-in-out'></span>
+                        <span
+                            className='cursor-pointer rounded-sm h-0.5 w-5 bg-gray-600 block content-none transition-all duration-1000 ease-in-out'></span>
+                        <span
+                            className='cursor-pointer rounded-sm h-0.5 w-6 bg-gray-600 block content-none transition-all duration-1000 ease-in-out'></span>
+                        <span
+                            className='cursor-pointer rounded-sm h-0.5 w-4 bg-gray-600 block content-none transition-all duration-1000 ease-in-out'></span>
                     </a>
                 </nav>
                 <main className='p-10 flex-1'>

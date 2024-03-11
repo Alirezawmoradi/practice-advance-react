9a3728067216c3cc5d920d4f6173ce9df7c04aa6
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-11 23:32:30 +0330

    toggleSidebar and language added to top nav for toggling sidebar

--- src/layouts/main-layout/top-nav.jsx
+++ src/layouts/main-layout/top-nav.jsx
@@ -1,12 +1,14 @@
 import ChangeLanguage from "../../components/change-language.jsx";
 import ChangeTheme from "../../components/change-theme.jsx";
+import {useAppContext} from "../../contexts/app/app-context.jsx";
 
 export const TopNav = () => {
+    const {toggleSidebar,language}=useAppContext()
     return (
         <nav
             className='relative flex py-3.5 px-0.5 items-center border-b-0 shadow justify-start bg-white dark:bg-dark align-middle transition-colors duration-500 ease-in-out'>
             <a className='cursor-pointer w-6 h-6 flex flex-col justify-center space-y-1.5 px-5'
-               onClick={() => setCollapseSidebar(!collapseSidebar)}>
+               onClick={toggleSidebar}>
                         <span
                             className='cursor-pointer rounded-sm h-0.5 w-5 bg-gray-600 dark:bg-gray-300 block content-none'></span>
                 <span

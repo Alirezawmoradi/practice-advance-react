40e3ae6d35c333693c8feff5f5e81636df22d178
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-11 23:44:51 +0330

    footer component completed

--- src/layouts/main-layout/footer.jsx
+++ src/layouts/main-layout/footer.jsx
@@ -1,8 +1,13 @@
 export const Footer = () => {
     return (
         <footer
-            className='block bg-white dark:bg-dark dark:border-gray-700 border-t border-solid border-b-gray-200 p-4 transition-colors duration-500 ease-in-out'>
-
+            className='block bg-white dark:bg-dark dark:border-gray-700 border-t border-solid border-b-gray-200 p-4 font-bold text-base text-gray-500 dark:text-gray-400 transition-colors duration-500 ease-in-out'>
+            <p className='flex self-center'>
+                © 2023 -
+                <a href='https://classbon.com' className='hover:underline hover:text-blue-600 hover:dark:text-blue-500'>
+                    کلاسبن
+                </a>
+            </p>
         </footer>
     )
 }

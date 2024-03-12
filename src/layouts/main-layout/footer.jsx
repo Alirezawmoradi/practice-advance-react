d8cbbecc78b62b2ee0892e5b594db2270a4967dd
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-12 23:04:19 +0330

    guard added to main layout to control permission to access to main page

--- src/layouts/main-layout/footer.jsx
+++ src/layouts/main-layout/footer.jsx
@@ -1,11 +1,14 @@
+import {useTranslation} from "react-i18next";
+
 export const Footer = () => {
+    const {t} = useTranslation();
     return (
         <footer
             className='block bg-white dark:bg-dark dark:border-gray-700 border-t border-solid border-b-gray-200 p-4 font-bold text-base text-gray-500 dark:text-gray-400 transition-colors duration-500 ease-in-out'>
             <p className='flex self-center'>
                 © 2023 -
                 <a href='https://classbon.com' className='hover:underline hover:text-blue-600 hover:dark:text-blue-500'>
-                    کلاسبن
+                    {t('footer.classbon')}
                 </a>
             </p>
         </footer>
--- src/layouts/main-layout/main-layout.jsx
+++ src/layouts/main-layout/main-layout.jsx
@@ -1,9 +1,14 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import {Sidebar} from "./sidebar.jsx";
 import {TopNav} from "./top-nav.jsx";
 import {Footer} from "./footer.jsx";
 
 export const MainLayout = () => {
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
+    if (!token) {
+        navigate('/login');
+    }
     return (
         <div className='items-stretch flex min-h-screen'>
             <Sidebar/>
